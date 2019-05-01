6a7d8237bc3e91e2db0e59ee32ccbfeb71fd0adb
lieby weintraub <liebahinda@gmail.com>
2019-05-01 00:11:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,6 +82,10 @@ def self.find_or_create_by(name:, breed:)
     dog_data=dog[0]
     dog=Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
   else 
-  
+  dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
     
 end 
